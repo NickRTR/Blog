8306ce2f24adb30ca3b288507af5a934098b4c1c
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-29 12:35:22 +0200

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<body>
+	<slot />
+</body>
+
+<style>
+	:global(body) {
+		margin: 0;
+		background-color: var(--background);
+		color: var(--color);
+	}
+
+	:global(:root) {
+		--accent: #ffc600;
+		--background: black;
+		--color: silver;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let posts;
+
+	console.log(posts);
+</script>
+
+<body>
+	<h1>Nick Reutlinger - Blog</h1>
+
+	<article>
+		{#each posts as post}
+			<div class="post">
+				<h1>{post.title}</h1>
+				<p>{post.shortDescription}</p>
+			</div>
+		{/each}
+	</article>
+</body>
+
+<style>
+	body {
+		text-align: center;
+	}
+</style>
