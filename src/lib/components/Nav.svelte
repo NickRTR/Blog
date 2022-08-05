6cd7d177e2e6f2a9e06fedb1151fc11b39a510fd
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-08-05 23:19:10 +0200

    Nav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,32 @@
+<nav>
+	<section>
+		<a href="/" title="Home">Home</a>
+	</section>
+	<a href="https://github.com/NickRTR/blog" title="GitHub">
+		<img src="/GitHub-light.png" alt="GitHub" />
+	</a>
+</nav>
+
+<style>
+	nav {
+		margin-top: 1rem;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	a {
+		font-weight: bold;
+		font-size: 1.2rem;
+	}
+
+	img {
+		width: 2rem;
+		height: 2rem;
+		filter: brightness(0.9);
+		transition: filter 0.1s ease-in-out;
+	}
+
+	a:hover img {
+		filter: brightness(1);
+	}
+</style>
