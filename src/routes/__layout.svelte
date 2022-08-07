573a6944302fb7bac9493972ec2ab6a091079eb6
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-08-07 20:31:31 +0200

    update border fix to only affect parent body

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,14 +11,15 @@
 
 <style>
 	body {
+		/* remove borders at top and bottom of mobile screens */
+		border: 0.1px solid transparent;
+
 		max-width: 700px;
 		font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 		padding-inline: 0.5rem;
 	}
 
 	:global(body) {
-		/* remove borders at top and bottom of mobile screens */
-		border: 0.1px solid transparent;
 		background-color: var(--background);
 		color: var(--color);
 		margin: auto;
